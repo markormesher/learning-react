0c7560eda50928da7940c664d1c21af0f59b37bb
Mark Ormesher <me@markormesher.co.uk>
2016-02-22 12:04:39 +0000

    Update "done" state on Firebase

--- 03-todo-app/src/list-item.jsx
+++ 03-todo-app/src/list-item.jsx
@@ -1,12 +1,37 @@
 var React = require('react');
+var Firebase = require('firebase');
+
+// firebase settings (IRL, this could come from a config file)
+var rootUrl = 'https://popping-torch-2380.firebaseio.com/';
 
 module.exports = React.createClass({
-	render: function () {
-		var item = this.props.item;
+	getInitialState: function () {
+		return {
+			text: this.props.item.text,
+			done: this.props.item.done
+		}
+	},
 
-		return <li className={item.done ? 'done' : ''}>
+	componentWillMount: function () {
+		this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
+	},
+
+	render: function () {
+		return <li className={'task' + (this.state.done ? ' done' : '')} onClick={this.handleClick}>
 			<i className="fa fa-square-o"/>
-			<span>{item.text}</span>
+			<span>{this.state.text}</span>
 		</li>
+	},
+
+	handleClick: function () {
+		var newDone = !this.state.done;
+
+		// change state
+		this.setState({
+			done: newDone
+		});
+
+		// update on backend
+		this.fb.update({done: newDone});
 	}
 });
