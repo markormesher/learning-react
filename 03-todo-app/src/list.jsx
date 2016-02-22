42ed7e156c8f7f3dcd89d9034a23b9130538e4e5
Mark Ormesher <me@markormesher.co.uk>
2016-02-22 10:20:03 +0000

    List UI

--- 03-todo-app/src/list.jsx
+++ 03-todo-app/src/list.jsx
@@ -9,9 +9,24 @@ module.exports = React.createClass({
 
 	renderList: function () {
 		if (this.props.items && Object.keys(this.props.items).length === 0) {
-			// empty
+			// empty (or still loading)
+			return <li className="empty">You have nothing to do!</li>
 		} else {
 			// list
+			var output = [];
+			for (var key in this.props.items) {
+				// get item
+				if (!this.props.items.hasOwnProperty(key)) continue;
+				var item = this.props.items[key];
+
+				// create UI
+				output.push(
+					<li key={key}>
+						{item.text}
+					</li>
+				);
+			}
+			return output;
 		}
 	}
 });
